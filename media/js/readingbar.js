a35740b7fd6f76116a5fe826a11e8bee4fdc5dab
jsanders <j53.sanders@gmail.com>
2023-01-30 16:04:15 +0100

    Update readingbar.js

--- media/js/readingbar.js
+++ media/js/readingbar.js
@@ -1,8 +1,17 @@
-window.addEventListener('scroll', readingBarScroll);
+(function () {
+  'use strict';
 
-function readingBarScroll() {
+  /**
+   * @copyright   (C) 2023 JG Sanders. All rights reserved.
+   * @license     GNU General Public License version 2 or later; see LICENSE.txt
+   */  
+  window.addEventListener('scroll', readingBarScroll);
+
+  function readingBarScroll() {
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
   var height    = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   var scrolled  = (winScroll / height) * 100;
   document.getElementById("readingbar").style.width = scrolled + "%";
 }
+
+})();
