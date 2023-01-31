a58b102ee46bf4d964dc5b5dae2afd986220f4c8
jsanders <j53.sanders@gmail.com>
2023-01-31 14:38:53 +0100

    Update readingbar.js

--- media/js/readingbar.js
+++ media/js/readingbar.js
@@ -8,9 +8,9 @@
   window.addEventListener('scroll', readingBarScroll);
 
   function readingBarScroll() {
-    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-    var height    = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    var scrolled  = (winScroll / height) * 100;
+    var scrolled = document.documentElement.scrollTop;
+    var height   = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (scrolled / height) * 100;
     document.getElementById("readingbar").style.width = scrolled + "%";
  };
 
