351082571f06132446ba9427f3e3905660d94156
jsanders <j53.sanders@gmail.com>
2023-02-03 09:34:25 +0100

    Update mod_readingbar.php

--- mod_readingbar.php
+++ mod_readingbar.php
@@ -3,9 +3,9 @@
  * @package     Reading Bar
  * @subpackage  mod_readingbar
  *
- * @author      JG Sanders
- * @copyright   Copyright (C) 2023 JG Sanders. All rights reserved.
- * @link        
+ * @author     JG Sanders
+ * @copyright  Copyright (C) 2023 JG Sanders. All rights reserved.
+ * @license    http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License Version 2 or Later       
  */
 
 // no direct access
