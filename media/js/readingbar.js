2701a1f90c856c08e885c0629e69495c92da8826
jsanders <j53.sanders@gmail.com>
2023-02-03 09:40:30 +0100

    Update readingbar.js

--- media/js/readingbar.js
+++ media/js/readingbar.js
@@ -3,7 +3,7 @@
 
   /**
    * @copyright   (C) 2023 JG Sanders. All rights reserved.
-   * @license     GNU General Public License version 2 or later; see LICENSE.txt
+   * @license     @license    http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License Version 2 or Later
    */  
   window.addEventListener('scroll', readingBarScroll);
 
