ce91d3e11aa2ebd5dea4a2f30117731e7a59dff2
jsanders <j53.sanders@gmail.com>
2023-03-16 14:23:36 +0100

    Some changes

--- script.php
+++ script.php
@@ -86,6 +86,8 @@ class mod_readingbarInstallerScript extends InstallerScript
      */
     public function preflight($type, $parent)
     {
+         echo Text::_('MOD_READINGBAR_INSTALLERSCRIPT_PREFLIGHT');
+
         // Check for the minimum PHP version before continuing
         if (!empty($this->minimumPhp) && version_compare(PHP_VERSION, $this->minimumPhp, '<'))
         {
@@ -102,8 +104,6 @@ class mod_readingbarInstallerScript extends InstallerScript
             return false;
         }
 
-        echo Text::_('MOD_READINGBAR_INSTALLERSCRIPT_PREFLIGHT');
-
         return true;
     }
 
@@ -117,14 +117,14 @@ class mod_readingbarInstallerScript extends InstallerScript
      */
     public function postflight($type, $parent)
     {
+        echo Text::_('MOD_READINGBAR_INSTALLERSCRIPT_POSTFLIGHT');
+
         $old = JPATH_ROOT . '/modules/mod_readingbar/mod_readingbar.php';
 
         if (file_exists($old))
         {
             File::delete($old);
         }
-
-        echo Text::_('MOD_READINGBAR_INSTALLERSCRIPT_POSTFLIGHT');
 
         return true;
     }
