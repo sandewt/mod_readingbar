9a8d1b0211e6f3fadeb32396a3f279b920268b41
jsanders <j53.sanders@gmail.com>
2023-03-17 07:57:35 +0100

    add note

--- script.php
+++ script.php
@@ -119,11 +119,11 @@ class mod_readingbarInstallerScript extends InstallerScript
     {
         echo Text::_('MOD_READINGBAR_INSTALLERSCRIPT_POSTFLIGHT');
 
-        $old = JPATH_ROOT . '/modules/mod_readingbar/mod_readingbar.php';
+        $old = JPATH_ROOT . '/modules/mod_readingbar/mod_readingbar.php'; // File needed here in old joomla structure
 
         if (file_exists($old))
         {
-            File::delete($old);
+            File::delete($old); // Delete file
         }
 
         return true;
